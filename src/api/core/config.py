e1bd97d1c2c32bd824a507c8f67e12d5554c9b39
kselig1 <you@example.com>
2026-02-03 12:34:09 -0800

    error running evals when env variables not set

--- src/api/core/config.py
+++ src/api/core/config.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings): 
     OPENAI_API_KEY: str  
     GROQ_API_KEY: str   
-    GOOGLE_API_KEY: str 
-    CO_API_KEY: str 
+    GOOGLE_API_KEY: str = ''
+    CO_API_KEY: str = ''
     QDRANT_API_KEY: str
     QDRANT_URL: str
 
