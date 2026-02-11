fa3084de0219fc980b4c6f14e8cd916f2743b094
kselig1 <198694969+kselig1@users.noreply.github.com>
2026-02-11 09:07:38 -0800

    added supabase AWS secret

--- ecr.sh
+++ ecr.sh
@@ -9,6 +9,6 @@ docker build -t amazon-be -f Dockerfile.fastapi --platform linux/amd64 .
 docker tag amazon-be:latest 912988925636.dkr.ecr.us-east-2.amazonaws.com/amazon-be:latest
 docker push 912988925636.dkr.ecr.us-east-2.amazonaws.com/amazon-be:latest
 
-docker build -t amazon-fe -f Dockerfile.streamlit --platform linux/amd64 .
-docker tag amazon-fe:latest 912988925636.dkr.ecr.us-east-2.amazonaws.com/amazon-fe:latest
-docker push 912988925636.dkr.ecr.us-east-2.amazonaws.com/amazon-fe:latest
+# docker build -t amazon-fe -f Dockerfile.streamlit --platform linux/amd64 .
+# docker tag amazon-fe:latest 912988925636.dkr.ecr.us-east-2.amazonaws.com/amazon-fe:latest
+# docker push 912988925636.dkr.ecr.us-east-2.amazonaws.com/amazon-fe:latest
